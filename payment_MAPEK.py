5f65ae878f7da1d00a86d199c3922b5334c51667
juank1400 <juank1400@gmail.com>
2023-10-27 16:19:46 -0500

    Adding Prometheus library instead of REST Call

--- payment_MAPEK.py
+++ payment_MAPEK.py
@@ -1,6 +1,7 @@
 import os
 from kubernetes import client, config
 import requests
+from prometheus_api_client import PrometheusConnect
 
 # Retrieve environment variables or set default values
 prometheus_url = os.environ.get('PROMETHEUS_URL', 'http://prometheus-k8s')
@@ -8,10 +9,18 @@ k8s_namespace = os.environ.get('K8S_NAMESPACE', 'juank1400-dev')
 deployment_name = os.environ.get('DEPLOYMENT_NAME', 'python-basic')
 
 def get_custom_metric_value():
+
+    # Crea una instancia de PrometheusConnect
+    prometheus = PrometheusConnect(url=prometheus_url)
+
+    # Realiza una consulta de ejemplo
+    query = "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace='{"+k8s_namespace+"}'}) by (pod)"
+    result = prometheus.custom_query(query)
+
     # Make an HTTP request to the Prometheus API using the environment variable
-    response = requests.get(prometheus_url+"/api/v1/query?query=sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace='{"+k8s_namespace+"}'}) by (pod)")
-    data = response.json()
-    return data['data']['result'][0]['value'][1]  # Extract the metric value
+    # response = requests.get(prometheus_url+"/api/v1/query?query=sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace='{"+k8s_namespace+"}'}) by (pod)")
+    # data = response.json()
+    return result['data']['result'][0]['value'][1]  # Extract the metric value
 
 def scale_deployment(namespace, deployment_name, replicas):
     try:
